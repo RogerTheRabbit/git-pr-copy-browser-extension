c094dba1d5af1beb806294b6e614eb64a4dc0a3f
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2023-01-28 23:03:51 -0500

    Since I can't match git.*.com, I'll accept all urls and then manually only look at git.*.com

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("WORKING");
+const matches_allowed = (url) => {
+  const patterns = [
+    /https:\/\/github.com.*/,
+    /https:\/\/git\..*\.com.*/,
+    /https:\/\/github\..*\.com.*/,
+  ];
+  return patterns.find((pattern) => pattern.test(url));
+};
+
+if (matches_allowed(window.location.href)) {
+  // Run code
+}
